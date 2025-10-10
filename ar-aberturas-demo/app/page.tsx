f5083e0458ea6752a76b7efbd3b553cde9819c7a
Juan Brusatti <juanbrusatti@gmail.com>
2025-10-10 19:12:51 -0300

    better handling when logging in

--- ar-aberturas-demo/app/page.tsx
+++ ar-aberturas-demo/app/page.tsx
@@ -1,10 +1,28 @@
+"use client"
+
 import { DashboardLayout } from "@/components/dashboard-layout"
 import { DashboardHome } from "@/components/dashboard-home"
+import { StockManagement } from "@/components/stock-management"
+import { useAuth } from "@/components/auth-provider"
+import { auth } from "@/lib/firebase"
+
+//obtener el usuario y dependiendo el usuario hacemos un switch para inicializar el dashboard o la seccion habilidata por el usuario
 
 export default function HomePage() {
-  return (
-    <DashboardLayout>
-      <DashboardHome />
-    </DashboardLayout>
-  )
+  const { user } = useAuth()
+  
+  switch (user?.role) {
+    case 'admin':
+      return <DashboardLayout><DashboardHome /></DashboardLayout>
+    case 'fabrica':
+      return <DashboardLayout><StockManagement /></DashboardLayout>
+    case 'ventas':
+      return <DashboardLayout><DashboardHome /></DashboardLayout>
+    case 'marketing':
+      return <DashboardLayout><DashboardHome /></DashboardLayout>
+    case 'colocador':
+      return <DashboardLayout><DashboardHome /></DashboardLayout>
+    default:
+      return <DashboardLayout><DashboardHome /></DashboardLayout>
+  }
 }

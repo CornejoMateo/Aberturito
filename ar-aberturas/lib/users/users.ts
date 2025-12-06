7aa252c9355d86d7d69be92daf91781b2430dc91
Mateo Cornejo <ccornejomateo@gmail.com>
2025-12-06 17:47:08 -0300

    Run prettier

--- ar-aberturas/lib/users/users.ts
+++ ar-aberturas/lib/users/users.ts
@@ -2,24 +2,30 @@ import { getSupabaseClient } from '../supabase-client';
 import bcrypt from 'bcryptjs';
 
 export type User = {
-    username: string;
-    role: string;
-    password?: string;
-}
+	username: string;
+	role: string;
+	password?: string;
+};
 
-export async function getUser(username: string, password: string): Promise<{ data: User | null; error: any }> {
+export async function getUser(
+	username: string,
+	password: string
+): Promise<{ data: User | null; error: any }> {
+	const supabase = getSupabaseClient();
+	const { data, error } = await supabase
+		.from('users')
+		.select('*')
+		.eq('username', username)
+		.single();
+	if (error) {
+		return { data: null, error };
+	}
 
-    const supabase = getSupabaseClient();
-    const { data, error } = await supabase.from('users').select('*').eq('username', username).single();
-    if (error) {
-        return { data: null, error };
-    }
-
-    const valid = await bcrypt.compare(password, data.password);
-    if (!valid) {
-        return { data: null, error: 'Contraseña incorrecta' };
-    } else {
-        data.password = undefined; // Remove password before returning
-        return { data, error: null };
-    }
+	const valid = await bcrypt.compare(password, data.password);
+	if (!valid) {
+		return { data: null, error: 'Contraseña incorrecta' };
+	} else {
+		data.password = undefined; // Remove password before returning
+		return { data, error: null };
+	}
 }

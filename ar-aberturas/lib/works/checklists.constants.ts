f3f9facb34f3f7526d08689067a64da17d9d1fa2
Mateo Cornejo <ccornejomateo@gmail.com>
2026-02-27 10:26:32 -0300

    Add more default checklists and opening types

--- ar-aberturas/lib/works/checklists.constants.ts
+++ ar-aberturas/lib/works/checklists.constants.ts
@@ -17,3 +17,37 @@ export const aluminioChecklistNames = [
 	'Accesorios',
 	'Herrajes',
 ];
+
+export const persianasChecklistNames = [
+	'Corroborar medidas de cortina',
+	'Colocar guías',
+	'Colocar planchuelas',
+	'Medir para corroborar rollo',
+	'Colocar el paño',
+	'Regular persiana',	
+	'Probar funcionamiento',
+]
+
+export const portonesChecklistNames = [
+	'Corroborar medida (que entre dentro de la hoja)',
+	'Amurar',
+	'Contrapesar',
+	'Sellar',
+	'Automatizar (corte arriba y corte abajo)',
+]
+
+export const mamparasChecklistNames = [
+	'Corroborar si pasa algún caño antes de perforar',
+	'Cortar perfilería (umbral, dintel, lateral)',
+	'Amurar perfiles',
+	'Montar herrajes',
+	'Montar vidrios',
+]
+
+export const vidrioChecklistNames = [
+	'Corroborar medidas',
+	'Colocar calzo',
+	'Montar',
+	'Colocar vidrio',
+	'Sellar'
+]

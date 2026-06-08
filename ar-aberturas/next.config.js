928005fb407b169045097185bbb24f0fc1f4a9f4
juanBrusatti <mjuanmolina123@gmail.com>
2026-06-08 11:04:57 -0300

    delete file

--- ar-aberturas/next.config.js
+++
@@ -1,7 +0,0 @@
-// Esto lo cree solamente porque me lo tiro la consola cuando queria acceder desde mi celular
-// Al parecer hay un nuevo parametro para configurar el host en el que corre la app
-// No cambia en nada la configuracion actual, solo es para que no me tire el warning
-// next.config.js
-module.exports = {
-  allowedDevOrigins: ['192.168.100.121'],
-}
